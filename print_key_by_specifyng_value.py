83fd097ce4a825ba182ba1178af3f652a0b991e3
Archana-Chinnaiah <66065120+Archana-Chinnaiah@users.noreply.github.com>
2020-06-12 08:12:46 -0700

    Update print_key_by_specifyng_value.py

--- print_key_by_specifyng_value.py
+++ print_key_by_specifyng_value.py
@@ -4,24 +4,7 @@ val_list = list(my_dict.values())
 continent_name=input("enter the continent name:")
 print(" The countries are")
 for key_list,val_list in my_dict.items():
-    if val_list==continent_name:
+   if (val_list.upper()==continent_name.upper()):
         print(key_list)
 
 
-#print(key_list[val_list.index(continent_name)]) 
-#print(key_list[val_list.index()]) 
-
-# one-liner 
-#print(list(my_dict.keys())[list(my_dict.values()).index(continent_name)])
-'''
-
-my_dict = {'Spain': 'Europe', 'Japan': 'Asia', 'India': 'Asia', 'Italy': 'Europe', 'Thailand': 'Asia', 'Sudan': 'Africa'} 
-for name,age in list.iteritems():
-    if age == search_age:
-            return name
-
->>> def method2(list,search_age):
-...     return [name for name,age in list.iteritems() if age == search_age]
-... 
->>> def method3(list,search_age):
-...     return list.keys()[list.values().index(search_age)]'''
