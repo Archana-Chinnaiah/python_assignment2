ea6639504ea6936367a543fd0da5585de76f4c07
Archana-Chinnaiah <66065120+Archana-Chinnaiah@users.noreply.github.com>
2020-06-12 08:16:38 -0700

    Update print_numbers_between_range.py

--- print_numbers_between_range.py
+++ print_numbers_between_range.py
@@ -3,5 +3,4 @@ last_num=int(input("enter the last number of the range"))
 for num in range(first_num,last_num+1): 
      if num%75 == 0 and num%100 == 0: 
                 print(str(num) + " ", end = "")
-     else: 
-                pass
+     
