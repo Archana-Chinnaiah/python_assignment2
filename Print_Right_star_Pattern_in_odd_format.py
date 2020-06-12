28de7ec32b2b49de863cb2e00406cabebea41c34
Archana C <archanachinnaiah@gmail.com>
2020-06-11 22:40:03 -0700

    print right star pattern in odd format

---
+++ Print_Right_star_Pattern_in_odd_format.py
@@ -0,0 +1,11 @@
+rows=int(input("Enter the number of rows:"))
+for row in range(1,rows+1):
+     if(row%2!=0):
+         for column in range(1,row+1):
+             print("*",end="")
+         print()   
+for row in range(rows, 0, -1):
+    if(row%2==0):
+        for column in range(0, row - 1):
+           print("*", end='')
+        print()
